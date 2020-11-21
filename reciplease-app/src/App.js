0efec4a7b0c0ade7241915ae34905fa0808a33db
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2020-11-21 11:48:14 -0500

    moving barcode to its own component

--- reciplease-app/src/App.js
+++ reciplease-app/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+import Ingredients from './components/Ingredients';
+import ScanBarcode from './components/ScanBarcode';
+import ScanFood from './components/ScanFood';
+import UploadBarcode from './components/UploadBarcode';
 import './App.css';
-import Quagga from 'quagga';
 
 function App() {
   return (
@@ -13,56 +15,12 @@ function App() {
         <h1>Welcome to Reciplease!</h1>
       </div>
 
-      <ImageSubmit/>
+      <UploadBarcode></UploadBarcode>
+
     </div>
   );
 }
 
-//Submission form for images (returns the code)
-function ImageSubmit() {
-  function processImage(base64Img) {
-    Quagga.decodeSingle({
-      decoder: {
-        readers: ["ean_reader"] // List of active readers
-      },
-      locate: true, // try to locate the barcode in the image
-      src: base64Img // or 'data:image/jpg;base64,' + data (the base64 image)
-    }, function(result){
-      if(result.codeResult) {
-        console.log("result", result.codeResult.code);
-      } else {
-        console.log("not detected");
-      }
-    });
-  }
 
-  //Coverts the selected file to base-64
-  function convertTo64(event) {
-    const file = document.getElementById('file').files[0];
-    var filename;
-    console.log('Test: ' + file);
-    if (FileReader && file) {
-      var fr = new FileReader();
-      console.log('Before onload');
-      fr.onloadend = function () {
-          filename = fr.result;
-          console.log('Test sequel: ' + filename);
-          processImage(filename);
-      }
-      fr.readAsDataURL(file);
-    }
-  }
-
-  return (
-    <form>
-      <div class="input-field">
-          <button type="button">Upload Barcode</button>
-          <button type="button" class="icon-barcode button scan" onClick={convertTo64}>&nbsp;Search</button>
-          <input type="file" id="file" capture/>
-      </div>
-    </form>
-  );
-
-}
 
 export default App;
--- reciplease-app/src/components/UploadBarcode.jsx
+++ reciplease-app/src/components/UploadBarcode.jsx
@@ -1,10 +1,52 @@
 import React, { useRef } from 'react';
+import Quagga from 'quagga';
 import './UploadBarcode.css';
 
 function UploadBarcode(props) {
+    //Submission form for images (returns the code)
+    function processImage(base64Img) {
+      Quagga.decodeSingle({
+        decoder: {
+          readers: ["ean_reader"] // List of active readers
+        },
+        locate: true, // try to locate the barcode in the image
+        src: base64Img // or 'data:image/jpg;base64,' + data (the base64 image)
+      }, function(result){
+        if(result.codeResult) {
+          console.log("result", result.codeResult.code);
+        } else {
+          console.log("not detected");
+        }
+      });
+    }
+  
+    //Coverts the selected file to base-64
+    function convertTo64(event) {
+      const file = document.getElementById('file').files[0];
+      var filename;
+      console.log('Test: ' + file);
+      if (FileReader && file) {
+        var fr = new FileReader();
+        console.log('Before onload');
+        fr.onloadend = function () {
+            filename = fr.result;
+            console.log('Test sequel: ' + filename);
+            processImage(filename);
+        }
+        fr.readAsDataURL(file);
+      }
+    }
+  
     return (
-        <div></div>
-    )
+      <form>
+        <div class="input-field">
+            <button type="button">Upload Barcode</button>
+            <button type="button" class="icon-barcode button scan" onClick={convertTo64}>&nbsp;Search</button>
+            <input type="file" id="file" capture/>
+        </div>
+      </form>
+    );
+
 }
 
 export default UploadBarcode;
