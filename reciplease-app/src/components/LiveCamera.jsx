13fbf94eaeb4b1e6f37120c7568093b3d0d27d93
c-kip <callumkipin@gmail.com>
2020-11-21 13:53:01 -0500

    Live camera runs (no barcode scanning yet)

--- reciplease-app/src/components/LiveCamera.jsx
+++ reciplease-app/src/components/LiveCamera.jsx
@@ -1,38 +1,39 @@
-import React, { useRef } from 'react';
+import React, { Component, useRef } from 'react';
 import './LiveCamera.css';
-import ScriptTag from 'react-script-tag'; //remove from npm
 import Quagga from 'quagga'; // ES6
 
-function LiveCamera(props) {
-    return (
-        <div id="interactive" class="viewport" onLoad={StartCamera}>
-        </div>
-    );
-}
-
-function StartCamera() {
-    console.log('Starting camera.');
-    Quagga.init({
-        inputStream : {
-          name : "Live",
-          type : "LiveStream",
-          target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
-        },
-        decoder : {
-          readers : ["code_128_reader"]
-        }
-        }, function(err) {
-          if (err) {
-              console.log(err);
-              return
-          }
-          console.log("Initialization finished. Ready to start");
-          Quagga.start();
-    });
+class LiveCamera extends Component {
+    render() {
+        return (
+            <div id="interactive" class="viewport">
+            </div>
+        );
+    }
     
-    Quagga.onDetected((data) => {
-        console.log('Code: ' + data.codeResult.code);
-    });
+    componentDidMount() {
+        console.log('Starting camera.');
+        Quagga.init({
+            inputStream : {
+              name : "Live",
+              type : "LiveStream",
+              target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+            },
+            decoder : {
+              readers : ["code_128_reader"]
+            }
+            }, function(err) {
+              if (err) {
+                  console.log(err);
+                  return
+              }
+              console.log("Initialization finished. Ready to start");
+              Quagga.start();
+        });
+        
+        Quagga.onDetected((data) => {
+            console.log('Code: ' + data.codeResult.code);
+        });
+    }
 }
 
 export default LiveCamera;
