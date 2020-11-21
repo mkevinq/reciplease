3fff76b4734b55ddb471067f70ec69082db8d290
Marc Kevin Quijalvo <33074023+mkevinq@users.noreply.github.com>
2020-11-21 01:03:15 -0500

    working barcode stuff, base64 string needs to be added

--- reciplease-app/src/App.js
+++ reciplease-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Quagga from 'quagga';
 
 function App() {
   return (
@@ -25,12 +26,27 @@ function App() {
 
 //Submission form for images (returns the code)
 function ImageSubmit() {
+  function processImage(event) {
+    Quagga.decodeSingle({
+      decoder: {
+        readers: ["ean_reader"] // List of active readers
+      },
+      locate: true, // try to locate the barcode in the image
+      src: 'data:image/jpg;base64,ojwoefjowiejf' // or 'data:image/jpg;base64,' + data
+    }, function(result){
+      if(result.codeResult) {
+        console.log("result", result.codeResult.code);
+      } else {
+        console.log("not detected");
+      }
+    });
+  }
   return (
     <form>
       <div class="input-field">
           <label for="isbn_input">EAN:</label>
           <input id="isbn_input" class="isbn" type="text" />
-          <button type="button" class="icon-barcode button scan">&nbsp;</button>
+          <button type="button" class="icon-barcode button scan" onClick={processImage}>&nbsp;</button>
           <input type="file" id="file" capture/>
       </div>
     </form>
