ae44e905f639371992724d8352789263df756cfb
c-kip <callumkipin@gmail.com>
2020-11-21 13:34:34 -0500

    Redoing camera code

--- reciplease-app/src/components/LiveCamera.jsx
+++ reciplease-app/src/components/LiveCamera.jsx
@@ -1,13 +1,38 @@
 import React, { useRef } from 'react';
 import './LiveCamera.css';
-import ScriptTag from 'react-script-tag';
+import ScriptTag from 'react-script-tag'; //remove from npm
+import Quagga from 'quagga'; // ES6
 
 function LiveCamera(props) {
     return (
-        <div id="interactive" class="viewport">
-            <ScriptTag src="../LiveVideo.js" />
+        <div id="interactive" class="viewport" onLoad={StartCamera}>
         </div>
     );
+}
+
+function StartCamera() {
+    console.log('Starting camera.');
+    Quagga.init({
+        inputStream : {
+          name : "Live",
+          type : "LiveStream",
+          target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+        },
+        decoder : {
+          readers : ["code_128_reader"]
+        }
+        }, function(err) {
+          if (err) {
+              console.log(err);
+              return
+          }
+          console.log("Initialization finished. Ready to start");
+          Quagga.start();
+    });
+    
+    Quagga.onDetected((data) => {
+        console.log('Code: ' + data.codeResult.code);
+    });
 }
 
 export default LiveCamera;
