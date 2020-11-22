ce0752af98da226e63ef00d5de8fc9bf838271ad
c-kip <callumkipin@gmail.com>
2020-11-21 20:24:47 -0500

    Moved 'hidden' submit button

--- reciplease-app/src/App.js
+++ reciplease-app/src/App.js
@@ -6,6 +6,7 @@ import LiveCamera from './components/LiveCamera';
 import './App.css';
 import logo from './assets/reciplease-logo.png';
 import reciplease from "./recipleaseBackend.js";
+import Quagga from 'quagga';
 
 // need to create a display recipes function to make a <Recipe> card for each recipe
 
@@ -26,6 +27,43 @@ function App() {
     }
   }
 
+  //Submission form for images (returns the code)
+  var hidden = 'hidden';
+  function processImage(base64Img) {
+    Quagga.decodeSingle({
+      decoder: {
+        readers: ["ean_reader"] // List of active readers
+      },
+      locate: true, // try to locate the barcode in the image
+      src: base64Img // or 'data:image/jpg;base64,' + data (the base64 image)
+    }, function(result){
+      if(result && result.codeResult) { //The first result is always NULL (not sure why though)
+        console.log("result", result.codeResult.code);
+        barcodeLookup(result.codeResult.code);
+      } else {
+        console.log("not detected");
+      }
+    });
+  }
+
+  //Coverts the selected file to base-64
+  function convertTo64(event) {
+    const file = document.getElementById('file').files[0];
+    var filename;
+    if (FileReader && file) {
+      var fr = new FileReader();
+      fr.onloadend = function () {
+          filename = fr.result;
+          processImage(filename);
+      }
+      fr.readAsDataURL(file);
+    }
+  }
+
+  function search (){
+      document.getElementById("search").style.visibility = "visible";
+  }
+
   return (
     <div className="App">
       <div className="content">
@@ -38,16 +76,24 @@ function App() {
           <div class="leftCollection">
             <p>Welcome to <i>reciplease</i>. Start finding awesome recipes today!</p>
             <div class="buttonCollection">
-              <UploadBarcode onButtonClick={barcodeLookup}/>
-                <div class='divider'/>
-                <button>Number 2</button>
-                <div class='divider'/>
-                <button>Number 3</button>
+              <form>
+                <div class="input-field">
+                    <label for="file" class="file-upload">
+                        Upload
+                    </label>
+                    <input type="file" id="file" onChange={search}/>
+                </div>
+              </form>
+              <div class='divider'/>
+              <button>Number 2</button>
+              <div class='divider'/>
+              <button>Number 3</button>
+              
             </div>
             <form method="get" action="/api/findRecipes">
             <textarea name="ingredients">
             </textarea>
-            <button type="submit">Get recipes!</button>
+            <button type="button" id="search" style={{visibility: "hidden"}}class="icon-barcode button scan" onClick={convertTo64}>&nbsp;Get recipes!</button>
           </form>
           </div>
           <LiveCamera onBarcodeDetection={barcodeLookup} />
