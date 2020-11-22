ca6ced69ce57cfdef7db11f1e2c711b81d1e8467
c-kip <callumkipin@gmail.com>
2020-11-22 00:01:57 -0500

    Modified convertTo64 further and combined w/ Kevin's image screenshot code

--- reciplease-app/src/App.js
+++ reciplease-app/src/App.js
@@ -65,7 +65,7 @@ function App() {
 
   //Submission form for images (returns the code)
   var hidden = 'hidden';
-  function processImage(base64Img) {
+  function processImage(base64Img, barcode) {
     Quagga.decodeSingle({
       decoder: {
         readers: ["ean_reader"] // List of active readers
@@ -73,7 +73,7 @@ function App() {
       locate: true, // try to locate the barcode in the image
       src: base64Img // or 'data:image/jpg;base64,' + data (the base64 image)
     }, function (result) {
-      if (result && result.codeResult) { //The first result is always NULL (not sure why though)
+      if (result && result.codeResult && barcode) { //The first result is always NULL (not sure why though)
         console.log("result", result.codeResult.code);
         barcodeLookup(result.codeResult.code);
         makeQuagga(barcodeLookup);
@@ -86,13 +86,13 @@ function App() {
   }
 
   //Coverts the given file to base-64
-  function convertTo64(event, file) {
+  function convertTo64(file, barcode) {
     var filename;
     if (FileReader && file) {
       var fr = new FileReader();
       fr.onloadend = function () {
         filename = fr.result;
-        processImage(filename);
+        processImage(filename, barcode);
       }
       fr.readAsDataURL(file);
     }
@@ -115,7 +115,7 @@ function App() {
                   Upload
                     </label>
                     <input type="file" id="file" onChange={(event) => {
-                      convertTo64(event, document.getElementById('file').files[0]);
+                      convertTo64(document.getElementById('file').files[0], true);
                     }}/>
                 </div>
               </form>
@@ -124,7 +124,7 @@ function App() {
               </textarea>
               <button type="button" id="search" class="icon-barcode button scan" onClick={findRecipes}>&nbsp;Get recipes!</button>
           </div>
-          <LiveCamera onBarcodeDetection={barcodeLookup} />
+          <LiveCamera onBarcodeDetection={barcodeLookup} base64Converter={convertTo64}/>
         </div>
 
         {/*Everything below the 'top collection'*/}
